7d8f0dc8d50d1b8890001388ba7e04331a62b954
Omakmusae <pak1208926@gmail.com>
2023-12-12 17:33:21 +0900

    package 작성 및 express 웹서버 구축

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello world');
+})
+
+app.listen(PORT);
+
+console.log(`Running on port ${PORT}`)
