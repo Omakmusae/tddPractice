a6d81564966df9065db4a7371cc1832fd7f1a2f0
sandbox <sandbox@quintet.co.kr>
2023-12-26 16:55:00 +0900

    에러 처리 주석 추가

--- controller/product.js
+++ controller/product.js
@@ -1,3 +1,5 @@
+//controller는 미들웨어 함수 부분만 구현해 놓음
+const { Mongoose } = require('mongoose');
 const productModel = require('../models/Product')
 
 
--- test/unit/product.test.js
+++ test/unit/product.test.js
@@ -1,41 +1,49 @@
 const productController = require('../../controller/product')
 const productModel = require('../../models/Product')
 const httpMocks = require('node-mocks-http')
-const newProduct = require('../data/new-product.json')
+const newProduct = require('../data/new-product.json');
+const Product = require('../../models/Product');
 
 
 productModel.create = jest.fn();
 
-let req, res, next;
+let req,res,next;
 
 beforeEach(() => {
-    req = httpMocks.createRequest();
+    req = httpMocks.createRequest(); // HTTP request argument to the middleware function
     //console.log(req);
-    res = httpMocks.createResponse();
-    next = jest.fn();
-}) 
-
+    res = httpMocks.createResponse();//HTTP response argument to the middleware function
+    next = jest.fn(); //Callback argument to the middleware function, 에러를 next()에 넣어서 전달할 수 있음
+})
 
 
 //describe 를 통해 그룹화 가능
 describe("Product Controller Create", () => {
     beforeEach(() => {
         req.body=newProduct;
-    }) 
+    })
 
-    test('should have a createProduct function', () => {
+    test('1. should have a createProduct function', () => {
         expect(typeof productController.createProduct).toBe("function");
     })
-    it("should call ProductModel.create", async () => {
-        console.log(productModel.create)
+    test("2. should call ProductModel.create", async () => {
+        
         await productController.createProduct(req,res, next);
         expect(productModel.create).toBeCalledWith(newProduct);
     })
-    it("should return 201 response code", async () => {
+
+    test("3. should return 201 response code", async () => {
         await productController.createProduct(req,res, next)
         expect(res.statusCode).toBe(201);
         expect(res._isEndCalled()).toBeTruthy();
     })
+
+    it("4. should return json body in response", async () => {
+        Product.create.mockReturnValue(newProduct);
+        await productController.createProduct(req,res,next);
+        expect(res._getJSONData()).toStrictEqual(newProduct);
+    })
+
     it("should return json body in response", async () => {
         productModel.create.mockReturnValue(newProduct)
         await productController.createProduct(req,res,next);
@@ -47,6 +55,9 @@ describe("Product Controller Create", () => {
         const rejectPromise = Promise.reject(errorMessage);
         productModel.create.mockReturnValue(rejectPromise); //임의로 에러 시, return 값 선언
         await productController.createProduct(req,res,next);
+        console.log("@@@@@@@@@@@",req.body)
+        console.log(errorMessage)
         expect(next).toBeCalledWith(errorMessage);
     })
+
 })
